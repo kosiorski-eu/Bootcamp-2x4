b0ba126e1392f4b6d02a5b72a16c4803c59a720d
kosiorski-eu <konrad@kosiorski.eu>
2017-01-17 14:48:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,25 @@
-(function($) {
+function createData(obj) {
 
-    $(document).ready(function() {
-		
-		//wszystkie elementy <div>, posiadające zarówno klasę “grid” oraz klasę “grid-12”
-		var div = $("div.grid, div.grid-12");
-		
-		//wszystkie elementy <a>, których atrybut “href" zaczyna się na “http”, znajdujące się w elemencie o klasie “nav”
-		var a = $(".nav a[href^='http']");
-		
-		//wszystkie elementy <input>, których typ to “radio” lub “checkbox” oraz dodatkowo nie są aktualnie zaznaczone (checked)
-		var input = $("input[type='radio'], input[type='checkbox']").not(":checked");
-		
-		//wyłącznie pierwszy element <p>, który jest pusty (nie zawiera dzieci) oraz znajduje się w elemencie <div> z identyfikatorem “text“
-		var p = $("div#text p:first:empty");
-		
-		//wszystkie elementy z klasą “pagination-item”, które nie są elementem <span>
-		var i = $(":not(span).pagination-item");
+    var data = {};
 
-    });
+    return {
+        set: function(key,value){
+            if(key&&value){
+                data[key]=value;
+            }
+        },
+        get:function(key){
+            return data[key];
+        }
+    };
 
-})(jQuery);
+}
 
+var data = createData();
 
+data.set("name", "Konrad");
 
+console.log( data.get("name") );
 
 
 
